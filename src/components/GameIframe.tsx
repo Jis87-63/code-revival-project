e5cd6c4094f875647b771ebc87f06383b055fa58
Jis87-63 <144544435+Jis87-63@users.noreply.github.com>
2026-02-19 12:33:54 +0200

    Update iframe source and title in GameIframe component

--- src/components/GameIframe.tsx
+++ src/components/GameIframe.tsx
@@ -2,9 +2,9 @@ const GameIframe = () => {
   return (
     <div className="w-full h-[800px] mt-4 rounded-lg overflow-hidden border border-foreground/5 shadow-md">
       <iframe
-        src="https://lotto24.co.mz/en?type=signup&btag=txkpeqevtnzifeaxaiujrrodef&affid=526103"
+        src="https://go.aff.oddsbest.co/uvvguo18"
         className="w-full h-full border-none"
-        title="TxunaPlay"
+        title="Lotto24"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen; payment; camera; microphone"
         allowFullScreen
         referrerPolicy="no-referrer-when-downgrade"
